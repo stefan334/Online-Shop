2516f9a0bdf7f390cef4eb41cbb9803aa1a4f944
stefan334 <stefan.ivan334@gmail.com>
2021-11-28 18:42:34 +0200

    Working on static gallery TODO: change gallery to be view

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const { create } = require('domain');
 const express = require('express');
+const fs = require('fs');
+const sharp = require('sharp');
 
 app = express();
 
@@ -6,9 +9,36 @@ app.set("view engine", "ejs");
 
 app.use("/Resources", express.static(__dirname + "/Resources"))
 
+function createImages() {
+    var buf = fs.readFileSync(__dirname + "/Resources/Jsons/galerie.json").toString("utf-8");
+    obImagini = JSON.parse(buf); //global
+    console.log(obImagini);
+    for (let imag of obImagini.imagini) {
+        console.log("a intrat")
+        let nume_imag, extensie;
+        [nume_imag, extensie] = imag.cale_imagine.split(".");
+        let dim_mic = 150;
+
+        imag.mic = `${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp`;
+
+        imag.mare = `${obImagini.cale_galerie}/${nume_imag}.jpg`
+
+        if (!fs.existsSync(imag.mic)) {
+            console.log(__dirname + "/" + imag.mare);
+            sharp(__dirname + "/" + imag.mare).resize(dim_mic).toFile(__dirname + "/" + imag.mic);
+        }
+
+    }
+
+
+}
+createImages();
 app.get(["/", "/index", "/home"], function(req, res) {
     console.log(req.url);
-    res.render("pagini/index.ejs", { ip: req.ip });
+    //------galerie
+
+
+    res.render("pagini/index.ejs", { ip: req.ip, imagini: obImagini.imagini, cale: obImagini.cale_galerie + "/" });
 
 });
 
