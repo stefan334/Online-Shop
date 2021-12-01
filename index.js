1b46f7e064522246371fb26030e1a462dcd0eb2f
stefan334 <stefan.ivan334@gmail.com>
2021-12-01 22:43:35 +0200

    static gallery working

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function createImages() {
         let nume_imag, extensie;
         [nume_imag, extensie] = imag.cale_imagine.split(".");
         let dim_mic = 150;
+        let dim_medie = 500;
 
         imag.mic = `${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp`;
+        imag.medie = `${obImagini.cale_galerie}/medie/${nume_imag}-${dim_medie}.webp`
 
         imag.mare = `${obImagini.cale_galerie}/${nume_imag}.jpg`
 
@@ -27,18 +29,57 @@ function createImages() {
             console.log(__dirname + "/" + imag.mare);
             sharp(__dirname + "/" + imag.mare).resize(dim_mic).toFile(__dirname + "/" + imag.mic);
         }
+        if (!fs.existsSync(imag.medie)) {
+            console.log(__dirname + "/" + imag.mare);
+            sharp(__dirname + "/" + imag.mare).resize(dim_medie).toFile(__dirname + "/" + imag.medie);
+        }
 
     }
 
 
 }
+
+
+function chooseImages() {
+    var date = new Date();
+    var today = Date.parse('01/01/2011 ' + date.getHours() + ':' + date.getMinutes());
+    console.log("Asta e azi " + today);
+    var timp1, timp2;
+    var imagini;
+    for (let i = 0; i < obImagini.imagini.length; i++) {
+
+        [timp1, timp2] = obImagini.imagini[i].timp.split("-");
+        console.log(today + " " + Date.parse('01/01/2011 ' + timp1));
+        if (today >= Date.parse('01/01/2011 ' + timp1) && today <= Date.parse('01/01/2011 ' + timp2)) {
+            // console.log("Am intrat aicea");
+        } else {
+            console.log("AM ELIMINAT UNUL");
+            obImagini.imagini.splice(i, 1);
+            i--;
+        }
+        // console.log("Aici am obimagini de i: " + obImagini.imagini[i]);
+
+
+
+    }
+}
 createImages();
+chooseImages();
 app.get(["/", "/index", "/home"], function(req, res) {
     console.log(req.url);
     //------galerie
 
 
     res.render("pagini/index.ejs", { ip: req.ip, imagini: obImagini.imagini, cale: obImagini.cale_galerie + "/" });
+
+});
+
+app.get("/gallery", function(req, res) {
+    console.log(req.url);
+    //------galerie
+
+
+    res.render("pagini/gallery.ejs", { imagini: obImagini.imagini, cale: obImagini.cale_galerie + "/" });
 
 });
 
